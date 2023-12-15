7ab184d726020693fa4f5d63358109abaab82376
Nagapranaya Vemula <nagapranaya.vemula@gmail.com>
2023-12-15 16:33:10 +0530

    Added bmi calculator and another screen

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+   
     }
-
-
+    @IBAction func heightSlider(_ sender: UISlider) {
+     
+        heightLabel.text = String(format:"%.2f" ,  sender.value) + "m"
+        
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        
+        weightLabel.text = String(format: "%.0f", sender.value) + "Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+       let bmi = weight / pow(height, 2)
+        print(bmi)
+        
+        let secondVc = SecondViewController()
+        secondVc.bmiValue = String(format: "%.1f" , bmi)
+         
+        self.present(secondVc, animated: true , completion: nil)
+        
+    }
 }
 
